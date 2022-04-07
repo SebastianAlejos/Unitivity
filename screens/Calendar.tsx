0fc22cc9fb590466ab3731277f556f7368b9a5d2
Sebastian Alejos <bastialejos@hey.com>
2022-04-06 22:08:19 -0400

    Added calendar slider for events, styling still required

--- screens/Calendar.tsx
+++ screens/Calendar.tsx
@@ -1,42 +1,32 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, View, Text, Button, Platform, ImageBackground } from 'react-native';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import CalendarStrip from 'react-native-slideable-calendar-strip';
+import { todayString } from 'react-native-calendars/src/expandableCalendar/commons';
+
 type CompProps = {
-    /* The props passed by navigation are much more complex,
-     *  but we are only using the navigate and goBack functions in this example
-     */
     navigation: { navigate: Function; goBack:Function};
   };
-  
   export default function Calendars(props: CompProps) {
     return (
-        <ImageBackground source={require("../assets/images/splash_1.png")} resizeMode="cover" style={styles.image}>
              <View style={styles.container}>
-             <Calendar
-                // Handler which gets executed on day press. Default = undefined
-                onDayPress={day => {
-                    console.log('selected day', day);
+             <Icon name="menu" size={44} style={styles.icon} onPress={() => props.navigation.navigate('Menu')}></Icon>
+             <CalendarStrip
+                style={styles.calendar}
+                selectedDate={'2022-04-07'}
+                onPressDate={(date) => {
+                    this.setState({ selectedDate: date });
                 }}
-                // Handler which gets executed on day long press. Default = undefined
-                onDayLongPress={day => {
-                    console.log('selected day', day);
+                onPressGoToday={(today) => {
                 }}
-                // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-                monthFormat={'yyyy MM'}
-                // Handler which gets executed when visible month changes in calendar. Default = undefined
-                onMonthChange={month => {
-                    console.log('month changed', month);
+                onSwipeDown={() => {
+                    alert('onSwipeDown');
                 }}
-                // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-                onPressArrowLeft={subtractMonth => subtractMonth()}
-                // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-                onPressArrowRight={addMonth => addMonth()}
-                // Disable left arrow. Default = false
-                enableSwipeMonths={true}
+                markedDate={['2018-05-04', '2018-05-15', '2018-06-04', '2018-05-01']}
+                weekStartsOn={0} // 0,1,2,3,4,5,6 for S M T W T F S, defaults to 0
                 />
     </View>
-</ImageBackground>
-        
     )};
  const styles = StyleSheet.create({
      image: {
@@ -45,9 +35,22 @@ type CompProps = {
         height:'100%',
         justifyContent: "center"
     },
+    icon:{
+        color:"#018786", 
+        alignSelf: 'flex-start',
+        position: 'absolute',
+        top:85,
+        left: 30,
+      },
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
       },
-});
+    calendar:{
+        borderRadius:90,
+        backgroundColor: '#FFFF',
+        position: 'relative', 
+        top:100, 
+    },
+});     
